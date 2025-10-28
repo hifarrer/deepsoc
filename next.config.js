a0a538e39e42e0728f5d75c17fc991087106bcb1
Your Name <your.email@example.com>
2025-10-28 16:10:11 -0500

    Fix image display issues: Add external image domains to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,106 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // App Router is enabled by default in Next.js 13+
+  images: {
+    remotePatterns: [
+      // Twitter/X images
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Reddit images
+      {
+        protocol: 'https',
+        hostname: 'i.redd.it',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'preview.redd.it',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'external-preview.redd.it',
+        port: '',
+        pathname: '/**',
+      },
+      // Instagram images
+      {
+        protocol: 'https',
+        hostname: 'scontent-*.cdninstagram.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'instagram.com',
+        port: '',
+        pathname: '/**',
+      },
+      // TikTok images
+      {
+        protocol: 'https',
+        hostname: 'p16-sign-va.tiktokcdn-us.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p16-sign.tiktokcdn-us.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p77-sign.tiktokcdn-us.com',
+        port: '',
+        pathname: '/**',
+      },
+      // YouTube images
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Facebook images
+      {
+        protocol: 'https',
+        hostname: 'scontent-*.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      // Generic fallback for any other domains
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
